854d68c0bc6dbd2019e2a63a387fc28d3ce15763
hiimtmac <hiimtmac+github@gmail.com>
2020-09-29 10:16:53 -0500

    swift 5.3 update

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftyGraphQL",
     platforms: [
-        .macOS(.v10_14)
+        .macOS(.v10_14), .iOS(.v11)
     ],
     products: [
         .library(name: "SwiftyGraphQL", targets: ["SwiftyGraphQL"])
